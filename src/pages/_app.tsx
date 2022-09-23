5ee9dc9708ad5ec26b86fd6e99dbf316662c7928
João Debussy <social.debussy@gmail.com>
2022-09-22 21:29:34 -0300

    feat: implementing api routes with next and authorization with next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,14 @@
-import { AppProps } from 'next/app';
+import { SessionProvider as NextAuthProvider } from 'next-auth/react';
 
 import '../styles/global.scss';
 
 import { Header } from '../components/Header';
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component,  pageProps: { session, ...pageProps} }) {
   return (
-    <>
+    <NextAuthProvider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
-  )
+    </NextAuthProvider>
+  );
 }
-
-export default MyApp;
