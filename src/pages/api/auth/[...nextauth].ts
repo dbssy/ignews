b71c3332c944b73ceb89ed237db80e98990eecc3
João Debussy <social.debussy@gmail.com>
2022-09-24 19:45:27 -0300

    feat: implementing stripe webhooks

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,23 +1,53 @@
-import { query as q } from 'faunadb';
-
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { query as q } from 'faunadb';
 
-import { fauna } from '../../../services/fauna'
+import { fauna } from '../../../services/fauna';
 
 export const authOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      authorization: {
-        params: {
-          scope: 'read:user'
-        }
-      }              
-    }),    
+    }),
   ],
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  'ref',
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                'active'
+              )
+            ])
+          )
+        );
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
+    },
     async signIn({ user, account, profile}) {
       const { email } = user;
 
